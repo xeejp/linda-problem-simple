e7f3abb240bbec8ce81581597b42828a271d4c90
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-28 17:22:04 +0900

    add DownloadButton

--- host/App.js
+++ host/App.js
@@ -8,9 +8,11 @@ import { fetchContents } from './actions'
 import PageStepper from './PageStepper'
 import Users from './Users'
 import EditQuestion from './EditQuestion'
+import DownloadButton from './DownloadButton'
 import Chart from './Chart'
 
-const mapStateToProps = ({}) => ({
+const mapStateToProps = ({ users, text, page }) => ({
+  users, text, page
 })
 
 class App extends Component {
@@ -25,6 +27,7 @@ class App extends Component {
   }
 
   render() {
+    const { users, text, page } = this.props
     return (
       <div>
         <PageStepper />
@@ -37,6 +40,19 @@ class App extends Component {
         <Users /><br />
         <Chart /><br />
         <EditQuestion />
+        <DownloadButton
+          fileName={"linda_problem_simple.csv"}
+          list={[
+            ["リンダ問題(簡易版)"],
+            ["実験日", new Date()],
+            ["登録者数", Object.keys(users).length],
+            ["ID", "回答"],
+          ].concat(
+            Object.keys(users).map(id => [id, (users[id].status == 'a')? text.answeres[0] : (users[id].status == 'b')? text.answeres[1] : (users[id].status == 'each')? text.answers[2] : "未回答"])
+          )}
+          style={{marginLeft: '2%'}}
+          disabled={page != "result"}
+        />
       </div>
     )
   }
--- host/EditQuestion.js
+++ host/EditQuestion.js
@@ -230,7 +230,7 @@ class EditQuestion extends Component {
         onTouchTap={this.reset.bind(this)}
       />,
     ]
-    return (<div>
+    return (<span>
       <FloatingActionButton onClick={this.handleOpen.bind(this)} disabled={page != "waiting"}>
          <ImageEdit />
       </FloatingActionButton>
@@ -262,7 +262,7 @@ class EditQuestion extends Component {
         autoHideDuration={2000}
         onRequestClose={this.handleRequestClose.bind(this)}
       />
-    </div>)
+    </span>)
   }
 }
 
