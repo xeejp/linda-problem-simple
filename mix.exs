aeff413a54776e3127e3672b108b107cb9ae4251
Ryohei HAYASHI <hayashi@xee.jp>
2016-10-20 21:27:17 +0900

    Applied naming rules.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule LindaProblemSimple.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :linda_problem_simple,
+    [app: :xee_linda_problem_simple,
      version: "0.1.0",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
